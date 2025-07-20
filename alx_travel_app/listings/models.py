ec1b305560dfa8f589dd63100c39db0a8e5f5b71
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-20 07:23:17 -0700

    position fix for classes

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -4,6 +4,16 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
 
 class Booking(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bookings')
@@ -16,13 +26,4 @@ class Booking(models.Model):
     def __str__(self):
         return f'{self.guest} booking at {self.listing}'
 
-class Listing(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    location = models.CharField(max_length=255)
-    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
-    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
-    created_at = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.title
